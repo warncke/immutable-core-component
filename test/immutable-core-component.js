77383831c05e5f8d9ab0d014217f2d0879b363cc
Ersun Warncke <ersun.warncke@gmail.com>
2017-09-30 22:57:51 -0400

    initial release

--- test/immutable-core-component.js
+++ test/immutable-core-component.js
@@ -2,16 +2,34 @@
 
 /* npm modules */
 const ImmutableCore = require('immutable-core')
+const ImmutableGlobal = require('immutable-global')
 const Promise = require('bluebird')
 const chai = require('chai')
+const sinon = require('sinon')
 
 /* application modules */
 const ImmutableCoreComponent = require('../lib/immutable-core-component')
 
 /* chai config */
 const assert = chai.assert
+sinon.assert.expose(chai.assert, { prefix: '' })
 
 describe('immutable-core-component', function () {
+
+    var sandbox
+
+    beforeEach(function () {
+        // reset global data
+        ImmutableCore.reset()
+        ImmutableCoreComponent.reset()
+        // create sinon sandbox
+        sandbox = sinon.sandbox.create()
+    })
+
+    afterEach(function () {
+        // clear sinon sandbox
+        sandbox.restore()
+    })
 
     it('should instantiate new component', function () {
         var component = new ImmutableCoreComponent({
@@ -22,12 +40,110 @@ describe('immutable-core-component', function () {
                 set: function (args) {},
             },
         })
+        // check properties
+        assert.strictEqual(component.ImmutableCoreComponent, true)
+        assert.strictEqual(component.class, 'ImmutableCoreComponent')
         // should create module
         var module = ImmutableCore.module('fooComponent')
+        // validate module
+        assert.strictEqual(module.meta.class, 'ImmutableCoreComponent')
         // module should have methods
         assert.isFunction(module.get)
         assert.isFunction(module.new)
         assert.isFunction(module.set)
+    })
+
+    it('should get component', function () {
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+        })
+        // get component
+        assert.deepEqual(component, ImmutableCoreComponent.component('foo'))
+    })
+
+    it('should check if component defined', function () {
+        // check false
+        assert.isFalse(ImmutableCoreComponent.hasComponent('foo'))
+        // create component
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+        })
+        // check true
+        assert.isTrue(ImmutableCoreComponent.hasComponent('foo'))
+    })
+
+    it('should throw error getting component that is not defined', function () {
+        assert.throws(() => {
+            ImmutableCoreComponent.component('foo')
+        }, 'foo component not defined')
+    })
+
+    it('should instantiate new component instance', async function () {
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+        })
+        // create new instance
+        var instance = await component.new()
+        // check properties
+        assert.strictEqual(instance.ImmutableCoreComponentInstance, true)
+        assert.strictEqual(instance.class, 'ImmutableCoreComponentInstance')
+    })
+
+    it('should call new method when creating new instance', async function () {
+        // create stub for new
+        var newStub = sandbox.stub().resolves()
+        // create new component
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+            server: {
+                new: newStub,
+            },
+        })
+        // create new instance
+        var instance = await component.new({
+            foo: true,
+            session: {},
+        })
+        // check that stub called
+        assert.calledOnce(newStub)
+        assert.calledWithMatch(newStub, {foo: true})
+    })
+
+    it('should set instance data and id from new return value', async function () {
+        // create stub for new
+        var newStub = sandbox.stub().resolves({
+            data: {foo: true},
+            id: 'foo',
+        })
+        // create new component
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+            server: {
+                new: newStub,
+            },
+        })
+        // create new instance
+        var instance = await component.new({
+            foo: true,
+            session: {},
+        })
+        // check instance
+        assert.deepEqual(instance.data, {foo: true})
+        assert.strictEqual(instance.id, 'foo')
+    })
+
+    it('should use component name for default id', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+        })
+        // create new instance
+        var instance = await component.new({
+            foo: true,
+            session: {},
+        })
+        // check id
+        assert.strictEqual(instance.id, 'foo')
     })
 
 })
---
+++ test/to-string.js
@@ -0,0 +1,224 @@
+'use strict'
+
+/* npm modules */
+const ImmutableCore = require('immutable-core')
+const ImmutableGlobal = require('immutable-global')
+const Promise = require('bluebird')
+const chai = require('chai')
+const handlebars = require('handlebars')
+const sinon = require('sinon')
+
+/* application modules */
+const ImmutableCoreComponent = require('../lib/immutable-core-component')
+
+/* chai config */
+const assert = chai.assert
+sinon.assert.expose(chai.assert, { prefix: '' })
+
+describe('immutable-core-component-instance toString', function () {
+
+    var sandbox
+
+    var newStub
+
+    beforeEach(function () {
+        // reset global data
+        ImmutableCore.reset()
+        ImmutableCoreComponent.reset()
+        // create sinon sandbox
+        sandbox = sinon.sandbox.create()
+        // create stub for new method
+        newStub = sandbox.stub().resolves({data: {foo: 'bar'}})
+    })
+
+    afterEach(function () {
+        // clear sinon sandbox
+        sandbox.restore()
+    })
+
+    it('should render template with toString', async function () {
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+            server: {new: newStub},
+            template: 'Hello {{foo}}',
+        })
+        // create new component instance
+        var instance = await component.new({session: {}})
+        // check that render includes template
+        assert.match(instance.toString(), '<div id="foo">Hello bar</div>')
+    })
+
+    it('should render template with partial', async function () {
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+            partials: {bar: 'bam'},
+            server: {new: newStub},
+            template: 'Hello {{foo}} {{>bar}}',
+        })
+        // create new component instance
+        var instance = await component.new({session: {}})
+        // check that render includes template
+        assert.match(instance.toString(), '<div id="foo">Hello bar bam</div>')
+    })
+
+    it('should render template with helper', async function () {
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+            helpers: {bar: () => 'bam'},
+            server: {new: newStub},
+            template: 'Hello {{foo}} {{bar}}',
+        })
+        // create new component instance
+        var instance = await component.new({session: {}})
+        // check that render includes template
+        assert.match(instance.toString(), '<div id="foo">Hello bar bam</div>')
+    })
+
+    it('should set default refreshInterval', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            client: {minify: true},
+            name: 'foo',
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), '"refreshInterval":0')
+    })
+
+    it('should set refreshInterval', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            client: {minify: true, refreshInterval: 1000},
+            name: 'foo',
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), '"refreshInterval":1000')
+    })
+
+    it('should set server hasGet/hasSet false', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            client: {minify: true},
+            name: 'foo',
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), '"server":{"hasGet":false,"hasSet":false}')
+    })
+
+    it('should set server hasGet/hasSet false', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            client: {minify: true},
+            name: 'foo',
+            server: {
+                get: () => {},
+                set: () => {},
+            },
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), '"server":{"hasGet":true,"hasSet":true}')
+    })
+
+    const hookNames = [
+        'preBind',
+        'preGet',
+        'preRefresh',
+        'preRender',
+        'preSet',
+        'postBind',
+        'postGet',
+        'postRefresh',
+        'postRender',
+        'postSet',
+    ]
+
+    hookNames.forEach(hookName => {
+        it(`should set ${hookName} hook`, async function () {
+            // create new component
+            var component = new ImmutableCoreComponent({
+                client: {
+                    hooks: {
+                        [hookName]: function () {return false}
+                    },
+                },
+                name: 'foo',
+            })
+            // create new instance
+            var instance = await component.new({session: {}})
+            // render
+            assert.match(instance.toString(), `component.${hookName} = function () {return false};`)
+        })
+    })
+
+    it('should register helpers', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            helpers: {
+                foo: function () {return 'foo'}
+            },
+            name: 'foo',
+            template: 'Hello {{foo}}',
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), `component.helpers['foo'] = function () {return 'foo'};`)
+    })
+
+    it('should register partials', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+            partials: {bar: 'bam'},
+            template: 'Hello {{foo}}',
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), `component.partials['bar'] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function`)
+    })
+
+    it('should set template', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            name: 'foo',
+            template: 'Hello {{foo}}',
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), `component.template = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function`)
+    })
+
+    it('should set css', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            css: '.foo {font-size: 1px}',
+            name: 'foo',
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), `<style>.foo {font-size: 1px}</style>`)
+    })
+
+    it('should set script', async function () {
+        // create new component
+        var component = new ImmutableCoreComponent({
+            script: 'alert("foo")',
+            name: 'foo',
+        })
+        // create new instance
+        var instance = await component.new({session: {}})
+        // render
+        assert.match(instance.toString(), `alert("foo")`)
+    })
+
+})
